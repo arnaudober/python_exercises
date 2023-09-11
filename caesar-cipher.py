4e70f80acfa26ec5d98bc66e6848f56f9233d620
Arnaud Ober <me@arnaudober.com>
2023-09-11 16:18:27 -0400

    Added decrypt method to the caesar cipher exercise

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,6 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -11,7 +11,18 @@ def encrypt(plain_text, shift):
         index = alphabet.index(char)
         cipher_text += alphabet[(index + shift) % 26]
 
-    return cipher_text
+    print(f"The encoded text is {cipher_text}")
 
-cipher_text = encrypt(text, shift)
-print(cipher_text) # ekxknkccvkqp
+def decrypt(cipher_text, shift):
+    plain_text = ""
+
+    for char in cipher_text:
+        index = alphabet.index(char)
+        plain_text += alphabet[(index - shift) % 26]
+
+    print(f"The decoded text is {plain_text}")
+
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
