8e76edeedeae13e580ff43a2f4255289354893aa
Arnaud Ober <me@arnaudober.com>
2024-01-03 20:06:00 +0530

    Snake game: Add keyboard listeners for snake direction

--- snake-game/snake.py
+++ snake-game/snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -26,3 +30,19 @@ class Snake:
             self.segments[segment_num].goto(new_x, new_y)
 
         self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
