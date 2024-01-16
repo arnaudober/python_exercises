33b1a0f713e1b02aaa7d194ba6dcbfccad39bbbd
Arnaud Ober <me@arnaudober.com>
2024-01-16 17:48:05 -0500

    Add ball and collision detections to pong game

--- pong/main.py
+++ pong/main.py
@@ -1,12 +1,16 @@
 from turtle import Screen
 from paddle import *
+from ball import *
+import time
 
+# Screen
 screen = Screen()
 screen.title("Pong")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
+# Paddles
 paddle1 = Paddle((350, 0))
 paddle2 = Paddle((-350, 0))
 
@@ -16,8 +20,23 @@ screen.onkey(paddle1.go_down, "Down")
 screen.onkey(paddle2.go_up, "w")
 screen.onkey(paddle2.go_down, "s")
 
+# Ball
+ball = Ball()
+
 game_is_on = True
 while game_is_on:
     screen.update()
+    time.sleep(0.1)
+    ball.move()
+
+    # Collisions detection
+    touches_top_or_bottom_wall = ball.ycor() > 280 or ball.ycor() < -280
+    if touches_top_or_bottom_wall:
+        ball.bounce_y()
+
+    touches_right_paddle = paddle1.distance(ball) < 50 and ball.xcor() > 320
+    touches_left_paddle = paddle2.distance(ball) < 50 and ball.xcor() < -320
+    if touches_right_paddle or touches_left_paddle:
+        ball.bounce_x()
 
 screen.exitonclick()
