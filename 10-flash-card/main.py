6e9643ca89e05b64b27f3a1f971a4c6ca2198bd8
Arnaud Ober <me@arnaudober.com>
2024-05-23 17:59:50 -0400

    Add flashcard functionality to main.py

--- 10-flash-card/main.py
+++ 10-flash-card/main.py
@@ -1,4 +1,7 @@
+import random
 from tkinter import Button, Canvas, Label, PhotoImage, Tk
+
+import pandas
 
 
 BACKGROUND_COLOR = "#B1DDC6"
@@ -6,6 +9,16 @@ BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# Read data
+data = pandas.read_csv("data/french_words.csv")
+data_dict = data.to_dict(orient="records")
+
+# Functions
+def next_card():
+  current_card = random.choice(data_dict)
+  canva.itemconfig(title_label, text="French", fill="black")
+  canva.itemconfig(word_label, text=current_card['French'], fill="black")
 
 # Canvas
 canva = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -19,11 +32,13 @@ canva.grid(row=0, column=0, columnspan=2)
 
 # Buttons
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR, highlightthickness=0, border=0)
+wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR, highlightthickness=0, border=0, command=next_card)
 wrong_button.grid(row=1, column=0)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, bg=BACKGROUND_COLOR, highlightthickness=0, border=0)
+right_button = Button(image=right_image, bg=BACKGROUND_COLOR, highlightthickness=0, border=0, command=next_card)
 right_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
