b55593e95c08dbe111df3a0f58d25a5dddc3914d
Arnaud Ober <me@arnaudober.com>
2024-06-13 18:46:09 -0400

    Add email functionality to send a quote of the week

--- 11-birthday-wisher/main.py
+++ 11-birthday-wisher/main.py
@@ -1,7 +1,18 @@
 import smtplib
+import datetime as dt
+import random
 import constants as constants
 
-with smtplib.SMTP("smtp-mail.outlook.com", port=587) as connection:
-  connection.starttls()
-  connection.login(user=constants.EMAIL_FROM, password=constants.PASSWORD)
-  connection.sendmail(from_addr=constants.EMAIL_FROM, to_addrs=constants.EMAIL_TO, msg="Subject:Hello\n\nThis is the body of my email")
+with open("quotes.txt", "r") as file:
+  quotes = file.readlines()
+
+  now = dt.datetime.now()
+  week_day = now.weekday()
+
+  if week_day == 0:
+    quote = random.choice(quotes)
+
+    with smtplib.SMTP("smtp-mail.outlook.com", port=587) as connection:
+      connection.starttls()
+      connection.login(user=constants.EMAIL_FROM, password=constants.PASSWORD)
+      connection.sendmail(from_addr=constants.EMAIL_FROM, to_addrs=constants.EMAIL_TO, msg="Subject:Quote of the week!\n\n" + quote)
