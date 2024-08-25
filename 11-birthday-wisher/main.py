3ab588090a13fca625becce5d8007cea65e691f7
Arnaud Ober <me@arnaudober.com>
2024-08-24 20:15:35 -0400

    Add birthday email sending functionality
    Refactor main.py to implement birthday email sending. The code now:
    - Checks today's date and matches it against the birthday list.
    - Retrieves birthday individuals from a CSV file.
    - Sends a birthday email using a simple hardcoded template.
    
    This change addresses the previous TODOs and prepares the code for
    further enhancement with dynamic templates and actual email sending
    functionalities.

--- 11-birthday-wisher/main.py
+++ 11-birthday-wisher/main.py
@@ -1,11 +1,53 @@
-import smtplib
-import datetime as dt
-import random
-import constants as constants
+import datetime
+
+import pandas
+
+
+def get_today():
+    now = datetime.datetime.now()
+    month = now.month
+    day = now.day
+    return month, day
+
+
+def get_birthdays_to_celebrate():
+    data = pandas.read_csv("birthdays.csv")
+    data_dict = data.to_dict(orient="records")
+    today_birthdays = {
+        (person["month"], person["day"]): {
+            "name": person["name"],
+            "email": person["email"],
+        }
+        for person in data_dict
+        if (person["month"], person["day"]) == get_today()
+    }
+
+    return today_birthdays
+
+
+def get_random_template():
+    return "Hello [NAME]!"
+
+
+def replace_variable_in_template(person_name, template):
+    return template.replace("[NAME]", person_name)
+
+
+def send_message(email_address, message):
+    print(f"Sending message to {email_address}")
+    print(f"Message: {message}")
+    return
+
 
 # TODO:
-# 1. Open the CSV file to get the information.
-# 2. For each birthday that is today:
-#   a. Randomly choose a template file in /letter_templates.
-#   b. Replace the variable with the name of the CSV file.
-#   c. Send the message to the email from the CSV file.
+# a. Randomly choose a template file in /letter_templates.
+# b. Replace the variable with the name of the CSV file.
+# c. Send the message to the email from the CSV file.
+birthdays_to_celebrate = get_birthdays_to_celebrate()
+for key in birthdays_to_celebrate:
+    person = birthdays_to_celebrate[key]
+    name = person["name"]
+    email = person["email"]
+    random_template = get_random_template()
+    emailToSend = replace_variable_in_template(name, random_template)
+    send_message(email, emailToSend)
